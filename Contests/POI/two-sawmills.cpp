f4e2652cb2635cdc30899bb5a543f6e356984949
COOLDOGE69WITHTHATBLACKDOIGE <anurag4629@gmail.com>
2024-01-02 07:59:26 +0530

    sawmill

--- Contests/POI/two-sawmills.cpp
+++ Contests/POI/two-sawmills.cpp
@@ -33,6 +33,7 @@ using namespace std;
 const int MOD = 1e9 + 7;
 const int INF = 1e17 + 1;
 const int maxN = 2e5 + 1;
+const int N = 2e4 + 1;
 
 void setIO(string name = ""){
 	ios_base::sync_with_stdio(0);
@@ -44,4 +45,60 @@ void setIO(string name = ""){
 	}
 }
 
-signed main(){}
+int dist[N];
+int cst[N];
+int psum[N];
+
+int dp[N][3];
+
+void dnc(int i, int l, int r, int optl, int optr){
+	if(l > r){
+		return;
+	}
+
+	int mid = (l+r)/2;
+
+	int opt = 1;
+	dp[mid][i] = INF;
+
+	forrange(j,optl,min(mid,optr)+1){
+		int ct = cst[mid] - cst[j] - ((psum[mid] - psum[j])*dist[j]);
+
+		if(dp[mid][i] > dp[j-1][i-1] + ct){
+			dp[mid][i] = dp[j-1][i-1] + ct;
+			opt = j;
+		}
+	}
+
+	dnc(i,l,mid-1,optl,opt);
+	dnc(i,mid+1,r,opt,optr);
+}
+
+signed main(){
+	setIO();
+	int n;
+	cin >> n;
+	int k = 2;
+	vi d(n); vi a(n);
+
+	for(int i = n-1; i > -1; --i){
+		cin >> a[i] >> d[i];
+	}
+
+	forrange(i,1,n+1){
+		dist[i] = dist[i-1] + d[i-1];
+		psum[i] = psum[i-1] + a[i-1];
+
+		cst[i] = cst[i-1] + (dist[i]*a[i-1]);
+	}
+
+	forrange(i,1,n+1){
+		dp[i][0] = cst[i];
+	}
+
+	forrange(i,1,k+1){
+		dnc(i,1,n,1,n);
+	}
+
+	cout << dp[n][k] << endl;
+}
